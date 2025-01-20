61709292fb700f694bdd93f5d30aca278944277f
kyungyoon <ruddbs803@gmail.com>
2025-01-20 06:50:13 +0000

    feat: add ga_deap_search.py and update __init__.py

--- hackathon/src/search/__init__.py
+++ hackathon/src/search/__init__.py
@@ -1,3 +1,4 @@
 from .bayesian_search import bayesian_search
 from .backprob_search import backprob_search
 from .eval_search_model import eval_search_model
+from .ga_deap_search import ga_deap_search
