75154ce436d61b67b69c079637dab5d84a6ff29d
Dangtae <youngtae0818@naver.com>
2025-01-21 11:23:32 +0900

    feat: Refactor file structure and improve data loading/processing
    - Renamed data_preprocessing.py to data_loader.py
      - Retained only data loading (load) and splitting (split) functions
    - Renamed cement_dataset.py to dataset.py
      - Implemented and stored cement_data and melb_data functions
    - Updated __init__.py
      - Properly exposed modules from dataset.py
    - Updated main.py
      - Adjusted imports and function calls to reflect the new file structure
    #12

--- hackathon/src/datasets/__init__.py
+++ hackathon/src/datasets/__init__.py
@@ -1,3 +1,4 @@
-from .cement_dataset import cement_data
+from .dataset import cement_data
+from .dataset import melb_data
 from .lightgbm_dataloader import lightgbm_load_data
 from .simpleNN_dataloader import simpleNN_load_data
