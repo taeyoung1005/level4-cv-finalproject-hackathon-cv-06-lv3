804a9f24b32588f3e86102092ebc323a8ad229ef
kimyoungseok3232 <rpfps3232@gmail.com>
2025-02-04 08:21:17 +0900

    feat: 스케일러에서 라벨 인코더 받아오도록 추가 #72

--- hackathon/src/preprocess/dynamic_scaling.py
+++ hackathon/src/preprocess/dynamic_scaling.py
@@ -1,10 +1,11 @@
 # src/preprocess/dynamic_scaling.py
 
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 from src.preprocess.analyze_distribution import analyze_distribution
 
-def dynamic_scaling(df: pd.DataFrame, numerical_cols: list) -> pd.DataFrame:
+
+def dynamic_scaling(df: pd.DataFrame, numerical_cols: list, scalers: dict) -> pd.DataFrame:
     """
     데이터 특성에 따라 동적으로 스케일링 방법을 선택.
     :param df: 입력 데이터프레임
@@ -12,7 +13,6 @@ def dynamic_scaling(df: pd.DataFrame, numerical_cols: list) -> pd.DataFrame:
     :return: 스케일링된 데이터프레임
     """
     distribution_info = analyze_distribution(df, numerical_cols)
-    scalers = {}
     
     for col in numerical_cols:
         if col not in df.columns:
