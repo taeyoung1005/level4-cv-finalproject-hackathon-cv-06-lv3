9d8896a318ea0a7ee63bc5b847546089fba60e94
Dangtae <youngtae0818@naver.com>
2025-02-05 02:33:23 +0000

    fix: update scaler initialization in dynamic_pipeline.py

--- hackathon/src/dynamic_pipeline.py
+++ hackathon/src/dynamic_pipeline.py
@@ -4,6 +4,7 @@ from src.preprocess.detect_features import detect_features
 from src.preprocess.dynamic_encoding import dynamic_encode
 from src.preprocess.dynamic_outlier import dynamic_outlier_removal
 from src.preprocess.dynamic_scaling import dynamic_scaling
+from src.preprocess.identity_scaler import IdentityScaler
 from src.preprocess.missing_values import (
     drop_high_missing_data,
     fill_missing_categorical,
@@ -31,7 +32,7 @@ def preprocess_dynamic(df: pd.DataFrame) -> pd.DataFrame:
     datetime_cols = feature_info['datetime']
     text_cols = feature_info['text']
     dtype_info = feature_info['dtypes']
-    scaler_info = {}
+    scaler_info = {col: IdentityScaler() for col in df.columns}
 
 
     # 2. 결측치 처리
